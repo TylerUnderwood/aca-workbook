d478f38960d5bebb03df9b93a61bb50650ea4d19
State Tyler <statetyler@State-Tylers-MacBook-Pro.local>
2019-10-15 19:51:49 -0500

    Update Yo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our updated server yo!')
+  res.send('Welcome to our updated server yo!!')
 })
 
 app.listen(port, () => {
